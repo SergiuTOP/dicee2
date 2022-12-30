9b2923446e0dbb7d8800b93e6d2a9b476fccc774
Sergiu <92325872+SergiuTOP@users.noreply.github.com>
2022-12-30 20:52:36 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+var randomNumber1 = Math.random();
+randomNumber1 = randomNumber1 * 6;
+randomNumber1 = Math.floor(randomNumber1) + 1;  // 1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //  dice1.png - dice6.png
+var randomImageSource = "images/" + randomDiceImage; //  images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.random();
+randomNumber2 = randomNumber2 * 6;
+randomNumber2 = Math.floor(randomNumber2) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png"; //  images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[1];
+image1.setAttribute("src", randomImageSource2);
+
+if(randomNumber1 > randomNumber2){
+  document.querySelector("h1").textContent = "🚩 Player 1 Wins!";
+} else if(randomNumber1 < randomNumber2){
+  document.querySelector("h1").textContent = "Player 2 Wins! 🚩";
+} else {
+  document.querySelector("h1").textContent = "Draw!";
+}
+
+
+function refreshPage(){
+    window.location.reload();
+}
